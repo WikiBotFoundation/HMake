91ded5bb19074b42e632daed49f9e1aac2a1491b
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-10 00:01:54 +0300

    Create texture.c

---
+++ 3d-print/texture.c
@@ -0,0 +1,21 @@
+//* A: Created By wikibot foundation//
+ #include java
+ #include tex
+ #include class
+ #include call
+ #include printer
+    viewModel="texture"
+      call_string_name="texture"
+   ViewFile="texture"
+        call_val("tex")
+           print("modelImage")
+           model("a-string_image(texture"))
+      texture("tex")
+          Model( texture = png)
+            string_post( c= c + function("texture")
+      texture(model( edit( = c= c+ function("model")
+   ViewFile="texture"
+       model( texture = c+ = function(abort.model)
+
+
+  
