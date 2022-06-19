9a5fed8b536f7aedf3a54361de94852b3d85a074
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2022-06-19 08:12:32 +0300

    Create main.c
    Program file

---
+++ 3d-print/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath.h>
+  class main(){
+   #include <elements.options>
+   #include <source.options>
+  }
+  int main(){
+    let char texture[] = Math.line(23, 10);
+  }
